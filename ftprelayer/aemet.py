70bdb1d53ce5bb94c3e47ac0252cec0b80b48ecd
Marcos Sánchez Provencio <rapto@arrakis.es>
2012-10-16 10:23:34 +0200

    Cambia aemet_rename a función porque config entiende que es una función

--- ftprelayer/aemet.py
+++ ftprelayer/aemet.py
@@ -4,7 +4,7 @@ import os.path
 import datetime
 
 
-class aemet_rename(object):
+def aemet_rename(path):
     u"""
     Renombra según reglas heredadas del Movedor de Jaime
 
@@ -13,34 +13,34 @@ class aemet_rename(object):
         >>> processor._new_name('foo')
         '2007_03_foo'
     """
-    now = datetime.datetime.utcnow # To mock in tests
-    def __call__(self, path):
-        basename = os.path.basename(path)
-        errors = []
-        if os.path.getsize(path) == 0:
-            errors.append('VACIO')
-        if path.lower().endswith('.zip'):
-            try:
-                zf = zipfile.ZipFile(path, 'r')
-                if zf.testzip():
-                    errors.append('ZIP_ERRONEO')
-            except zipfile.BadZipfile:   
-                    errors.append('NO_ES_ZIP')
-        if basename.startswith('10'):
-            new_name = 'Algunos-SYNOPs-' + basename[4:6] + self.now().strftime('-%Y-%m-%d-') \
-                    + basename[6:8] + '00.txt'
-        elif basename.startswith('SYNOP'):
-            new_name = 'S' + basename[5:6] + self.now().strftime('-%Y-%m-%d-') \
+    basename = os.path.basename(path)
+    errors = []
+    if os.path.getsize(path) == 0:
+        errors.append('VACIO')
+    if path.lower().endswith('.zip'):
+        try:
+            zf = zipfile.ZipFile(path, 'r')
+            if zf.testzip():
+                errors.append('ZIP_ERRONEO')
+        except zipfile.BadZipfile:   
+                errors.append('NO_ES_ZIP')
+    if basename.startswith('10'):
+        new_name = 'Algunos-SYNOPs-' + basename[4:6] + aemet_rename.now().strftime('-%Y-%m-%d-') \
                 + basename[6:8] + '00.txt'
-        elif basename.startswith('W'):
-            new_name = basename[0:8] + self.now().strftime('_%Y-%m-%d_%H-%M-%S') \
-                + '.txt'
-        elif basename.startswith('SYHOAU'):
-            new_name = 'SHA' + self.now().strftime('-%Y-%m-%d-') \
-                + basename[6:8] + '00.txt'
-        else:
-            new_name = basename
-        if errors:
-            new_name += '.' + '.'.join(errors)
-        with open(path) as f:
-            yield new_name, f.read()
+    elif basename.startswith('SYNOP'):
+        new_name = 'S' + basename[5:6] + aemet_rename.now().strftime('-%Y-%m-%d-') \
+            + basename[6:8] + '00.txt'
+    elif basename.startswith('W'):
+        new_name = basename[0:8] + aemet_rename.now().strftime('_%Y-%m-%d_%H-%M-%S') \
+            + '.txt'
+    elif basename.startswith('SYHOAU'):
+        new_name = 'SHA' + aemet_rename.now().strftime('-%Y-%m-%d-') \
+            + basename[6:8] + '00.txt'
+    else:
+        new_name = basename
+    if errors:
+        new_name += '.' + '.'.join(errors)
+    with open(path) as f:
+        yield new_name, f.read()
+
+aemet_rename.now = datetime.datetime.utcnow # To mock in tests
